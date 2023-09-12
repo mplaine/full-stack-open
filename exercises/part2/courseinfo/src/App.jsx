f3f3f63a7059182a0664d23ac2edb283d2876eb2
Markku Laine <markku.laine@gmail.com>
2023-09-12 10:06:06 +0300

    feat(exercise2.2,exercise2.3): add my solution for the exercises 2.2 and 2.3

--- exercises/part2/courseinfo/src/App.jsx
+++ exercises/part2/courseinfo/src/App.jsx
@@ -4,10 +4,13 @@ const Content = ({ parts }) => parts.map(part => <Part key={part.id} part={part}
 
 const Part = ({ part }) => <p>{part.name} {part.exercises}</p>
 
+const Total = ({ sum }) => <p><strong>total of {sum} exercises</strong></p>
+
 const Course = ({ course }) =>
   <div>
     <Header course={course.name} />
     <Content parts={course.parts} />
+    <Total sum={course.parts.reduce((total, part) => total + part.exercises, 0)} />
   </div>
 
 const App = () => {
@@ -29,6 +32,11 @@ const App = () => {
         name: 'State of a component',
         exercises: 14,
         id: 3
+      },
+      {
+        name: 'Redux',
+        exercises: 11,
+        id: 4
       }
     ]
   }
