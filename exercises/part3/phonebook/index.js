1a21f7c141ff1f7d6e1437ecb953f8f023b92868
Markku Laine <markku.laine@gmail.com>
2023-09-18 11:55:33 +0300

    feat(exercise3.5): add my solution for the exercise 3.5

--- exercises/part3/phonebook/index.js
+++ exercises/part3/phonebook/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
   { 
     "id": 1,
@@ -23,6 +25,11 @@ let persons = [
     "number": "39-23-6423122"
   }
 ]
+
+const generateId = () => {
+  const max = 1000000
+  return Math.floor(Math.random() * max)
+}
 
 app.get('/api/persons', (request, response) => {
   response.json(persons)
@@ -51,6 +58,26 @@ app.delete('/api/persons/:id', (request, response) => {
   else {
     response.status(404).end()
   }
+})
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: 'content missing'
+    })
+  }
+
+  const person = {
+    id: generateId(),
+    name: body.name,
+    number: body.number
+  }
+
+  persons = persons.concat(person)
+
+  response.json(person)
 })
 
 app.get('/info', (request, response) => {
