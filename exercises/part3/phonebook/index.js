9655e74db310c1e65e11f19ce83ee4ccd3a459ae
Markku Laine <markku.laine@gmail.com>
2023-09-18 11:27:53 +0300

    feat(exercise3.2): add my solution for the exercise 3.2

--- exercises/part3/phonebook/index.js
+++ exercises/part3/phonebook/index.js
@@ -28,6 +28,13 @@ app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
 
+app.get('/info', (request, response) => {
+  const line1 = `<p>Phonebook has info for ${persons.length} people</p>`
+  const line2 = `<p>${new Date()}</p>`
+  const content = line1 + line2
+  response.send(content)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
