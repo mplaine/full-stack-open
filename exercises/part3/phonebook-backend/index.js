e55d668344fa1572a124fd5dff295dbf19475685
Markku Laine <markku.laine@gmail.com>
2023-09-25 17:50:02 +0300

    feat(exercise3.16): add my solution for the exercise 3.16

--- exercises/part3/phonebook-backend/index.js
+++ exercises/part3/phonebook-backend/index.js
@@ -1,7 +1,8 @@
-require('dotenv').config()
 const cors = require('cors')
 const express = require('express')
+const app = express()
 const morgan = require('morgan')
+require('dotenv').config()
 const Person = require('./models/person')
 
 morgan.token('body', function getRequestBody(req) {
@@ -10,9 +11,35 @@ morgan.token('body', function getRequestBody(req) {
   }
 })
 
-const app = express()
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({
+      error: 'malformatted id'
+    })
+  }
+
+  next(error)
+}
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({
+    error: 'unknown endpoint'
+  })
+}
+
 app.use(cors())
 app.use(express.json())
+app.use(requestLogger)
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 app.use(express.static('dist'))
 
@@ -39,11 +66,12 @@ let persons = [
   }
 ]
 
-app.get('/api/persons', (request, response) => {
-  Person.find({}).
-    then(persons => {
-      response.json(persons)
+app.get('/api/persons', (request, response, next) => {
+  Person.find({})
+    .then(retrievedPersons => {
+      response.json(retrievedPersons)
     })
+    .catch(error => next(error))
 })
 
 app.get('/api/persons/:id', (request, response) => {
@@ -58,7 +86,7 @@ app.get('/api/persons/:id', (request, response) => {
   }
 })
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndRemove(request.params.id)
     .then(deletedPerson => {
       if (deletedPerson) {
@@ -68,9 +96,10 @@ app.delete('/api/persons/:id', (request, response) => {
         response.status(404).end()
       }
     })
+    .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   const body = request.body
 
   if (!body.name || !body.number) {
@@ -94,6 +123,7 @@ app.post('/api/persons', (request, response) => {
     .then(createdPerson => {
       response.json(createdPerson)
     })
+    .catch(error => next(error))
 })
 
 app.get('/info', (request, response) => {
@@ -102,6 +132,9 @@ app.get('/info', (request, response) => {
   const content = line1 + line2
   response.send(content)
 })
+
+app.use(unknownEndpoint)
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
