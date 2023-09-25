df0d30d9ee51eb481643dbc519fe21d847e492c8
Markku Laine <markku.laine@gmail.com>
2023-09-25 18:15:24 +0300

    feat(exercise3.17): add my solution for the exercise 3.17

--- exercises/part3/phonebook-backend/index.js
+++ exercises/part3/phonebook-backend/index.js
@@ -99,6 +99,27 @@ app.delete('/api/persons/:id', (request, response, next) => {
     .catch(error => next(error))
 })
 
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: 'The name or number is missing'
+    })
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson);
+    })
+    .catch(error => next(error))
+})
+
 app.post('/api/persons', (request, response, next) => {
   const body = request.body
 
