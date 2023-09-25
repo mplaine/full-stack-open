7b9df3e8d32a83ac51c22a4de158fd9dabc906c6
Markku Laine <markku.laine@gmail.com>
2023-09-25 12:29:39 +0300

    feat(exercise3.11): add my solution for the exercise 3.11

--- exercises/part3/phonebook-backend/index.js
+++ exercises/part3/phonebook-backend/index.js
@@ -12,6 +12,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+app.use(express.static('dist'))
 
 let persons = [
   { 
