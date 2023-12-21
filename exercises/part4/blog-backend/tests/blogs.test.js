a8f720268d433397040124b0846b997516f818e1
Markku Laine <markku.laine@gmail.com>
2023-12-21 15:08:06 +0200

    feat(exercise4.4): add my solution for the exercise 4.4

--- exercises/part4/blog-backend/tests/blogs.test.js
+++ exercises/part4/blog-backend/tests/blogs.test.js
@@ -6,3 +6,58 @@ test('dummy returns one', () => {
   const result = listHelper.dummy(blogs)
   expect(result).toBe(1)
 })
+
+describe('total likes', () => {
+  const listWithZeroBlogs = []
+  const listWithOneBlog = [
+    {
+      _id: '658334c7075b466243501149',
+      title: 'Performance audit: Lego.com',
+      author: 'Tammy Everts',
+      url: 'https://www.speedcurve.com/blog/web-performance-audit-lego/',
+      likes: 5,
+      __v: 0
+    },
+  ]
+  const listWithManyBlogs = [
+    {
+      _id: '658334c7075b466243501149',
+      title: 'Performance audit: Lego.com',
+      author: 'Tammy Everts',
+      url: 'https://www.speedcurve.com/blog/web-performance-audit-lego/',
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: '6583350a075b46624350114b',
+      title: 'NEW! December product update',
+      author: 'Cliff Crocker',
+      url: 'https://www.speedcurve.com/blog/december-2023-update/',
+      likes: 99,
+      __v: 0
+    },
+    {
+      _id: '65833530075b46624350114d',
+      title: 'Mobile INP performance: The elephant in the room',
+      author: 'Cliff Crocker',
+      url: 'https://www.speedcurve.com/blog/core-web-vitals-inp-mobile/',
+      likes: 2,
+      __v: 0
+    }
+  ]
+
+  test('of empty list array is zero', () => {
+    const result = listHelper.totalLikes(listWithZeroBlogs)
+    expect(result).toBe(0)
+  })
+
+  test('when list has only one blog equals the likes of that', () => {
+    const result = listHelper.totalLikes(listWithOneBlog)
+    expect(result).toBe(5)
+  })
+
+  test('of a bigger list is calculated right', () => {
+    const result = listHelper.totalLikes(listWithManyBlogs)
+    expect(result).toBe(106)
+  })
+})
