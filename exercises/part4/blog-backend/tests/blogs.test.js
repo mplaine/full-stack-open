ec6da862e5438ac28e2d46ada639f08f3e417f15
Markku Laine <markku.laine@gmail.com>
2023-12-21 15:30:55 +0200

    feat(exercise4.5): add my solution for the exercise 4.5

--- exercises/part4/blog-backend/tests/blogs.test.js
+++ exercises/part4/blog-backend/tests/blogs.test.js
@@ -61,3 +61,75 @@ describe('total likes', () => {
     expect(result).toBe(106)
   })
 })
+
+describe('favorite blog', () => {
+  const listWithZeroBlogs = []
+  const listWithOneBlog = [
+    {
+      _id: '658334c7075b466243501149',
+      title: 'Performance audit: Lego.com',
+      author: 'Tammy Everts',
+      url: 'https://www.speedcurve.com/blog/web-performance-audit-lego/',
+      likes: 5,
+      __v: 0
+    },
+  ]
+  const listWithManyBlogs = [
+    {
+      _id: '658334c7075b466243501149',
+      title: 'Performance audit: Lego.com',
+      author: 'Tammy Everts',
+      url: 'https://www.speedcurve.com/blog/web-performance-audit-lego/',
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: '6583350a075b46624350114b',
+      title: 'NEW! December product update',
+      author: 'Cliff Crocker',
+      url: 'https://www.speedcurve.com/blog/december-2023-update/',
+      likes: 99,
+      __v: 0
+    },
+    {
+      _id: '65833530075b46624350114d',
+      title: 'Mobile INP performance: The elephant in the room',
+      author: 'Cliff Crocker',
+      url: 'https://www.speedcurve.com/blog/core-web-vitals-inp-mobile/',
+      likes: 2,
+      __v: 0
+    },
+  ]
+
+  test('of empty list array is zero', () => {
+    const result = listHelper.favoriteBlog(listWithZeroBlogs)
+    const expected = null
+    expect(result).toEqual(expected)
+  })
+
+  test('when list has only one blog equals the likes of that', () => {
+    const result = listHelper.favoriteBlog(listWithOneBlog)
+    const expected = {
+      _id: '658334c7075b466243501149',
+      title: 'Performance audit: Lego.com',
+      author: 'Tammy Everts',
+      url: 'https://www.speedcurve.com/blog/web-performance-audit-lego/',
+      likes: 5,
+      __v: 0
+    }
+    expect(result).toEqual(expected)
+  })
+
+  test('of a bigger list is calculated right', () => {
+    const result = listHelper.favoriteBlog(listWithManyBlogs)
+    const expected = {
+      _id: '6583350a075b46624350114b',
+      title: 'NEW! December product update',
+      author: 'Cliff Crocker',
+      url: 'https://www.speedcurve.com/blog/december-2023-update/',
+      likes: 99,
+      __v: 0
+    }
+    expect(result).toEqual(expected)
+  })
+})
