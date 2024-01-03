d400e405b58934af7a1dbbaafa8c5b03ee42a41b
Markku Laine <markku.laine@gmail.com>
2024-01-03 10:53:33 +0200

    feat(exercise5.14): add my solution for the exercise 5.14

--- exercises/part5/blog-frontend/src/components/Blog.test.js
+++ exercises/part5/blog-frontend/src/components/Blog.test.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 describe('<Blog />', () => {
+  const user = {
+    token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InRldmVydHMiLCJpZCI6IjY1ODcyNzZjYzljMDQyNDcxNWQ1NjY2OCIsImlhdCI6MTcwMzc4OTk5NiwiZXhwIjoxNzAzNzkzNTk2fQ.x9LzF-lWO6Y9f0PGsVrTbd4sTrB7ZOqohV1Cqlv-1Xo',
+    username: 'teverts',
+    name: 'Tammy Everts'
+  }
   const blog = {
     title: 'NEW! December product update',
     author: 'Cliff Crocker',
@@ -16,10 +22,12 @@ describe('<Blog />', () => {
     },
     id: '658c6a6a12d561b9ad496925'
   }
+  const updateMockHandler = jest.fn()
+  const deleteMockHandler = jest.fn()
   let container
 
   beforeEach(() => {
-    container = render(<Blog blog={blog} />).container
+    container = render(<Blog blog={blog} updateBlog={updateMockHandler} deleteBlog={deleteMockHandler} user={user} />).container
   })
 
   test('renders title and author', async () => {
@@ -29,5 +37,13 @@ describe('<Blog />', () => {
   test('does not render URL and likes by default', async () => {
     expect(container).not.toHaveTextContent(blog.url)
     expect(container).not.toHaveTextContent(blog.likes)
+  })
+
+  test('after clicking the "view" button, shows URL and likes', async () => {
+    const user = userEvent.setup()
+    const button = screen.getByText('view')
+    await user.click(button)
+    expect(container).toHaveTextContent(blog.url)
+    expect(container).toHaveTextContent(`likes ${blog.likes}`)
   })
 })
