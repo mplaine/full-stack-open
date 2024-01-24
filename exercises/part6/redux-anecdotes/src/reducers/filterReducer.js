719303e3a502b4207f4c98e62054213c02d462fa
Markku Laine <markku.laine@gmail.com>
2024-01-24 11:46:59 +0200

    feat(exercise6.10): add my solution for the exercise 6.10

--- exercises/part6/redux-anecdotes/src/reducers/filterReducer.js
+++ exercises/part6/redux-anecdotes/src/reducers/filterReducer.js
@@ -1,20 +1,16 @@
-const filterReducer = (state = '', action) => {
-  // console.log('state now: ', state)
-  // console.log('action', action)
+import { createSlice } from '@reduxjs/toolkit'
 
-  switch (action.type) {
-    case 'SET_FILTER':
+const initialState = ''
+
+const filterSlice = createSlice({
+  name: 'filter',
+  initialState,
+  reducers: {
+    updateFilter(state, action) {
       return action.payload
-    default:
-      return state
+    }
   }
-}
+})
 
-export const updateFilter = filter => {
-  return {
-    type: 'SET_FILTER',
-    payload: filter,
-  }
-}
-
-export default filterReducer
+export const { updateFilter } = filterSlice.actions
+export default filterSlice.reducer
