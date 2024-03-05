c736cde51c7cc220b0a93169de92b9f599fc41dc
Markku Laine <markku.laine@gmail.com>
2024-03-05 09:42:06 +0200

    feat(exercises): add my solution for the exercise 13.6

--- exercises/part13/blog-backend/controllers/blogs.js
+++ exercises/part13/blog-backend/controllers/blogs.js
@@ -3,25 +3,40 @@ const middleware = require('../utils/middleware')
 const { Blog } = require('../models')
 
 blogsRouter.get('/', async (request, response) => {
-  const retrievedBlogs = await Blog.findAll()
-  response.json(retrievedBlogs)
+  const blogs = await Blog.findAll()
+  response.json(blogs)
 })
 
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
 
   try {
-    const createdBlog = await Blog.create(body)
-    response.status(201).json(createdBlog)
+    const blog = await Blog.create(body)
+    response.status(201).json(blog)
   } catch (error) {
     return response.status(400).json({ error })
   }
 })
 
-blogsRouter.delete('/:id', middleware.blogFinder, async (request, response) => {
-  const deletedCount = await Blog.destroy({ where: { id: request.params.id } })
+blogsRouter.delete('/:id', async (request, response) => {
+  const id = request.params.id
+  const deletedCount = await Blog.destroy({ where: { id: id } })
+
   if (deletedCount === 1) {
     response.status(204).end()
+  } else {
+    response.status(404).end()
+  }
+})
+
+blogsRouter.put('/:id', middleware.blogFinder, async (request, response) => {
+  const blog = request.blog
+  const body = request.body
+
+  if (blog) {
+    blog.likes = body.likes === undefined ? 0 : body.likes
+    await blog.save()
+    response.json(blog)
   } else {
     response.status(404).end()
   }
