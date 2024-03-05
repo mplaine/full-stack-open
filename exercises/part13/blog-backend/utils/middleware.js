396d86751e3348dc26ab18ce683c0c0a63b78596
Markku Laine <markku.laine@gmail.com>
2024-03-05 13:26:58 +0200

    feat(exercises): add my solution for the exercise 13.9

--- exercises/part13/blog-backend/utils/middleware.js
+++ exercises/part13/blog-backend/utils/middleware.js
@@ -14,7 +14,7 @@ const errorHandler = (error, request, response, next) => {
     })
   } else if (error.name === 'SequelizeValidationError') {
     return response.status(400).json({
-      error: error.message
+      error: error.errors.map((error) => error.message)
     })
   }
 
