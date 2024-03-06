a119fb30200cc6fab3d75e1eb68962aef65429c2
Markku Laine <markku.laine@gmail.com>
2024-03-06 17:40:15 +0200

    feat(exercises): add my solution for the exercise 13.17

--- exercises/part13/blog-backend/utils/db.js
+++ exercises/part13/blog-backend/utils/db.js
@@ -1,4 +1,5 @@
 const Sequelize = require('sequelize')
+const { Umzug, SequelizeStorage } = require('umzug')
 const config = require('./config')
 const logger = require('./logger')
 
@@ -19,6 +20,7 @@ const sequelize = new Sequelize(config.DATABASE_URL, {
 const connectToDatabase = async () => {
   try {
     await sequelize.authenticate()
+    await runMigrations()
     logger.info('connected to the database')
   } catch (err) {
     logger.info('failed to connect to the database')
@@ -28,4 +30,27 @@ const connectToDatabase = async () => {
   return null
 }
 
-module.exports = { connectToDatabase, sequelize }
+const migrationConf = {
+  migrations: {
+    glob: 'migrations/*.js'
+  },
+  storage: new SequelizeStorage({ sequelize, tableName: 'migrations' }),
+  context: sequelize.getQueryInterface(),
+  logger: console
+}
+
+const runMigrations = async () => {
+  const migrator = new Umzug(migrationConf)
+  const migrations = await migrator.up()
+  console.log('Migrations up to date', {
+    files: migrations.map((mig) => mig.name)
+  })
+}
+
+const rollbackMigration = async () => {
+  await sequelize.authenticate()
+  const migrator = new Umzug(migrationConf)
+  await migrator.down()
+}
+
+module.exports = { connectToDatabase, sequelize, rollbackMigration }
